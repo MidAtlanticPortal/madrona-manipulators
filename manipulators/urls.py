e48c7c389128bbc2459a4f999df496d5cb961451
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:28:26 -0700

    django 2 url patterns update

--- manipulators/urls.py
+++ manipulators/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
+from django.urls import re_path, include
+from . import views
 
-urlpatterns = patterns('manipulators.views',
-    (r'^test/$', 'testView'),
-    (r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', 'mpaManipulatorList'),
-    url(r'^([A-Za-z0-9_,]+)/$', 'multi_generic_manipulator_view', name='manipulate'),
-    url(r'^$', 'multi_generic_manipulator_view', {'manipulators': None}, name='manipulate-blank'),
-    url(r'^/$', 'multi_generic_manipulator_view', {'manipulators': None}, name='manipulate-blank'),
-)
+urlpatterns = [
+    #'manipulators.views',
+    re_path(r'^test/$', views.testView),
+    re_path(r'^list/([A-Za-z0-9_,]+)/([A-Za-z0-9_,]+)/$', views.mpaManipulatorList),
+    re_path(r'^([A-Za-z0-9_,]+)/$', views.multi_generic_manipulator_view, name='manipulate'),
+    re_path(r'^$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
+    re_path(r'^/$', views.multi_generic_manipulator_view, {'manipulators': None}, name='manipulate-blank'),
+]
